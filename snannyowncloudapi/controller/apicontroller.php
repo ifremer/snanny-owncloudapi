1c092a3ba2443d6b8e822c4954d131776a51920a
Arnaud Thorel <athorel@asi-rennes.fr>
2016-06-23 18:15:45 +0200

    Ajout d'un parse du deploymentid

--- snannyowncloudapi/controller/apicontroller.php
+++ snannyowncloudapi/controller/apicontroller.php
@@ -109,10 +109,15 @@ class ApiController extends Controller
     public function sensorML($id, $pretty = false, $startTime = null, $endTime = null)
     {
         $system = null;
-        if (preg_match('"^[\d]*$"', $id)) {
+		if (preg_match('"^[\d]*$"', $id)) {
             $system = $this->systemMapper->getById($id);
         } else {
-            $systems = $this->systemMapper->getByUuidAndDate($id, $startTime, $endTime, false);
+			if (strpos($id, '_') > -1) {
+				$exploded_id = explode("_", $id);
+				$id = $exploded_id[0];
+				$startTime = $exploded_id[1];
+				$endTime = $exploded_id[2];
+			}
 
             if ($systems != null && count($systems) == 1) {
                 $system = $systems[0];
